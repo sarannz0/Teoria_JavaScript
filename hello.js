929dd3e2f4235876127e8c190ba555121b08e9c7
Sara Nuñez <balaguera2008@gmail.com>
2025-08-13 12:55:04 -0500

    feat: :sparkles: Operadores

--- hello.js
+++ hello.js
@@ -46,3 +46,28 @@ mostrar(number *=2);
 mostrar(number /=5);
 mostrar(number %=2);
 mostrar(number /0);
+
+//Operadores de igualdad
+mostrar("Igualdad: "+ (5 == '5'))
+mostrar("Igualdad: "+ (5 === '5'))
+mostrar("Desigualdad: "+ (5 != '5'))
+mostrar("Desigualdad estricta: "+ (5 !== '5'))
+mostrar("Mayor que: "+ (5 > '5'))
+mostrar("Mayor igual que: "+ (5 >= '5'))
+mostrar("Menor que: "+ (5 < '5'))
+mostrar("Menor igual que: "+ (5 <= '5'))
+mostrar("Concatenacion: "+ (5 + '5'))
+
+let a,b,c;
+a=b=c=1+20
+mostrar(a);
+mostrar(b);
+mostrar(c);
+
+//Operadores logicos
+
+mostrar("Operador y logico(And): " +(true && true))
+mostrar("Operador y logico(And): " +(5 != 5 && true))
+mostrar("Operador y logico(Or): " +(5 > 5 || 2<6))
+mostrar("Operador y logico(NOT): " + !(5>=5))
+mostrar("Operador y logico(NOT): " + !(5>5))
