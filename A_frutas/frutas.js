790d2f1df4aea383fac02dae65079756bccb4030
Sara NUñez <balaguera2008@gmail.com>
2025-08-19 12:54:19 -0500

    feat: :sparkles: Actividad Frutas

---
+++ A_frutas/frutas.js
@@ -0,0 +1,8 @@
+// Set -> Evitar elementos dupicados las ordena ...
+const numeros = new Set ([1,5,2,3,2]); // ->
+numeros.size
+numeros.has(xelemento) // -> true / false
+numeros.delete(xelemento) // -> true / false
+numeros.add(xelemento) // -> Set
+numeros.clear()
+
